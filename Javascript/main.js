88252dac7cb3d02eede00d67648da1636cbe0ee4
Timothy Sharkey <Techne3@gmail.com>
2019-08-28 11:20:57 -0600

    Card design and new text

--- Javascript/main.js
+++ Javascript/main.js
@@ -7,4 +7,21 @@ function burgerDisplay() {
     } else {
       x.style.display = "block";
     }
-  }
+    
+}
+
+// function burgerOpen(){
+//     let y = document.getElementById("burgerMenu");
+//     if (screen.width > 1026){
+//         y.style.display==="block"
+//     }
+// }
+
+
+
+window.addEventListener("resize", function(){
+    if (screen.width > 926){
+        document.getElementById("burgerMenu").style.display==="block"
+     }
+   
+  });

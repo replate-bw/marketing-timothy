c721e20a37d4cd2ee34c3334f968ee83099de8c4
Timothy Sharkey <Techne3@gmail.com>
2019-08-28 14:02:29 -0600

    New nav

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,18 +10,3 @@ function burgerDisplay() {
     
 }
 
-// function burgerOpen(){
-//     let y = document.getElementById("burgerMenu");
-//     if (screen.width > 1026){
-//         y.style.display==="block"
-//     }
-// }
-
-
-
-window.addEventListener("resize", function(){
-    if (screen.width > 926){
-        document.getElementById("burgerMenu").style.display==="block"
-     }
-   
-  });
